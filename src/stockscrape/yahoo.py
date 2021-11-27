acae9eabacf58ed3873f679b5cb2783beb568617
rohan0x <rohansingh07294@gmail.com>
2021-11-27 02:33:05 -0500

    Added numerous scrape methods

--- src/stockscrape/yahoo.py
+++ src/stockscrape/yahoo.py
@@ -1,21 +1,67 @@
 from requests import get
 
 def get_price(stock_name : str):
-    stock_name = stock_name.upper()
+    url = f"https://finance.yahoo.com/quote/{stock_name}?p={stock_name}&.tsrc=fin-srch"
+    scraped_text = get(url).text
 
-    try:
-        url = f"https://finance.yahoo.com/quote/{stock_name}?p={stock_name}&.tsrc=fin-srch"
-        scraped_text = get(url).text
+    _ = scraped_text.index("\"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)\"")
+    scraped_text = scraped_text[_:_+1000]
 
-        scraped_text = scraped_text[
-            scraped_text.index("\"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)\""):
-        ]
+    return float(
+        scraped_text[
+            scraped_text.index("47")+4:scraped_text.index("</s")
+        ].replace(',', '')
+    )
 
-        price = scraped_text[scraped_text.index("47")+4:scraped_text.index("</span")]
-        price = float(price.replace(',', ''))
+def get_percent_change(stock_name : str):
+    url = f"https://finance.yahoo.com/quote/{stock_name}?p={stock_name}"
+    scraped_text = get(url).text
 
-        return price
-        
-    except:
-        print(f"Stock {stock_name} not found.")
-        return False
+    _ = scraped_text.index("Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px)")
+    scraped_text = scraped_text[_+50:_+100]
+    scraped_text = scraped_text[scraped_text.index("48")+4:]
+
+    return float(
+        scraped_text[
+            scraped_text.index("(")+1:scraped_text.index("%")
+        ].replace(',','')
+    )
+
+def get_change(stock_name : str):
+    url = f"https://finance.yahoo.com/quote/{stock_name}?p={stock_name}"
+    scraped_text = get(url).text
+
+    _ = scraped_text.index("Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px)")
+    scraped_text = scraped_text[_+50:_+100]
+
+    return float(
+        scraped_text[
+            scraped_text.index("48")+4:scraped_text.index("(")-1
+        ].replace(',','')
+    )
+
+def get_open(stock_name : str):
+    url = f"https://finance.yahoo.com/quote/{stock_name}?p={stock_name}&.tsrc=fin-srch"
+    scraped_text = get(url).text
+
+    _ = scraped_text.index("OPEN-value")
+    scraped_text = scraped_text[_+56:_+170]
+
+    return float(
+        scraped_text[
+            scraped_text.index("\">")+2:scraped_text.index("</s")
+        ].replace(',','')
+    )
+
+def get_previous_close(stock_name : str):
+    url = f"https://finance.yahoo.com/quote/{stock_name}?p={stock_name}&.tsrc=fin-srch"
+    scraped_text = get(url).text
+
+    _ = scraped_text.index("PREV_CLOSE-value")
+    scraped_text = scraped_text[_+56:_+170]
+
+    return float(
+        scraped_text[
+            scraped_text.index("\">")+2:scraped_text.index("</s")
+        ].replace(',','')
+    )
